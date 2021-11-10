89249ff2bac0c063ff2f3ccabf63a98fc5d51d6b
Muhammad Nabeel <munabeel@microsoft.com>
2021-11-10 14:28:56 -0800

    OpenTelemetry updates for client and Iot Edge.

--- ClientApp/ClientApp/Program.cs
+++ ClientApp/ClientApp/Program.cs
@@ -6,9 +6,18 @@ using System.Threading.Tasks;
 using OpenTelemetry;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+using OpenTelemetry.Context.Propagation;
 using System.Collections.Generic;
 using System.Diagnostics;
 using Azure.Monitor.OpenTelemetry.Exporter;
+using System.Net.Http.Headers;
+
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Text.RegularExpressions;
+using OpenTelemetry.Context;
 
 namespace ClientApp
 {
@@ -16,22 +25,27 @@ namespace ClientApp
     {
         private static ServiceClient serviceClient;
         private static ActivitySource source = new ActivitySource("Sample.DistributedTracing", "1.0.0");
+        private static readonly TextMapPropagator propagator = Propagators.DefaultTextMapPropagator;
+        private static readonly Action<HttpRequestMessage, string, string> HttpRequestMessageHeaderValueSetter = (request, name, value) => request.Headers.Add(name, value);
+        //private readonly HttpClientInstrumentationOptions options;
+
         static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            var connectionString = "[your-iothub-connection-string]";
+            
+            var connectionString = "HostName=new-shalimar.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=vy6Y3RZqHoAOBUfPxpNIDpb92cLxAhrjWgrKLyRj2nk=";
             serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
-            await InvokeMethodAsync();
 
 
             var resourceAttributes = new Dictionary<string, object> { { "service.name", "my-service" }, { "service.namespace", "my-namespace" }, { "service.instance.id", "my-instance" } };
             var resourceBuilder = ResourceBuilder.CreateDefault().AddAttributes(resourceAttributes);
 
+
+
             using var tracerProvider = Sdk.CreateTracerProviderBuilder()
                 .SetResourceBuilder(resourceBuilder)
-                .AddSource("Demo.DemoServer")
-                .AddSource("Demo.DemoClient")
+
                 .AddSource("Sample.DistributedTracing")
                 .AddConsoleExporter()
                 .AddAzureMonitorTraceExporter(o =>
@@ -40,21 +54,44 @@ namespace ClientApp
                 })
                 //.AddConsoleExporter()
                 .Build();
+            Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+            using (Activity activity = source.StartActivity("Sample.DistributedTracing"))
+            {
+                
+                ActivityContext activityContextToInject = default;
+                if(activity != null)
+                {
+                    activityContextToInject = activity.Context;
+                }
+                else if (Activity.Current != null)
+                {
+                    activityContextToInject = Activity.Current.Context;
+                }
 
-            await DoSomeWork("banana", 8);
-            Console.WriteLine("Example work done");
-            // All the functions below simulate doing some arbitrary work
+                var traceParentKeyValue = new KeyValuePair<string, string>("traceparent", activity.Id);
+                var traceStateKeyValue = new KeyValuePair<string, string>("tracestate", "congo=t61rcWkgMzE");
+                var headerDictionary = new Dictionary<string, string>();
+                headerDictionary.Add("traceparent", activity.Id);
+                headerDictionary.Add("tracestate", "congo=t61rcWkgMzE");
 
+                //propagator.Inject(activityContextToInject, headerDictionary);
+                //propagator.Inject(new PropagationContext(activityContextToInject, Baggage.Current),headerDictionary);
+                //await DoSomeWork("banana", 8);
+                Console.WriteLine("Example work done");
+                // All the functions below simulate doing some arbitrary work
+                await InvokeMethodUsingHTTPAsync(activity);
 
-            serviceClient.Dispose();
+                serviceClient.Dispose();
+            }
         }
+
+
         static async Task DoSomeWork(string foo, int bar)
         {
-            using (Activity activity = source.StartActivity("SomeWork"))
-            {
-                await StepOne();
-                await StepTwo();
-            }
+            
+            await StepOne();
+            await StepTwo();
+            
         }
 
         static async Task StepOne()
@@ -68,18 +105,57 @@ namespace ClientApp
         }
 
         // Invoke the direct method on the device, passing the payload
-        private static async Task InvokeMethodAsync()
+        private static async Task InvokeMethodAsync(Activity activity)
         {
             var methodInvocation = new CloudToDeviceMethod("SetTelemetryInterval")
             {
                 ResponseTimeout = TimeSpan.FromSeconds(30),
             };
-            methodInvocation.SetPayloadJson("10");
-
+            methodInvocation.SetPayloadJson("12");
+            ActivityContext activityContextToInject = default;
+            if (activity != null)
+            {
+                activityContextToInject = activity.Context;
+            }
+            else if (Activity.Current != null)
+            {
+                activityContextToInject = Activity.Current.Context;
+            }
+            propagator.Inject<CloudToDeviceMethod>(new PropagationContext(activity.Context, Baggage.Current), methodInvocation, null);// (Action<CloudToDeviceMethod, string, string>)HttpRequestMessageHeaderValueSetter);
             // Invoke the direct method asynchronously and get the response from the simulated device.
             var response = await serviceClient.InvokeDeviceMethodAsync("new-nabeel-percept-device", "edgesolution", methodInvocation);
 
             Console.WriteLine($"\nResponse status: {response.Status}, payload:\n\t{response.GetPayloadAsJson()}");
+        }
+
+        private static async Task InvokeMethodUsingHTTPAsync(Activity activity)
+        {
+            // Command to generate token
+            // az iot hub generate-sas-token -n [iot-hub-name] --du 1003600
+            Console.WriteLine($"\nReceived Activity:{ activity }");
+            Console.WriteLine($"\nReceived Activity.TraceId:{ activity.TraceId }");
+            Console.WriteLine($"\nReceived Activity.TraceStateString:{ activity.TraceStateString }");
+            Console.WriteLine($"\nReceived Activity.SpanId:{ activity.SpanId }");
+            Console.WriteLine($"\nReceived Activity.Baggage:{ activity.Baggage }");
+            Console.WriteLine($"\nReceived Activity.Context:{ activity.Context }");
+            Console.WriteLine($"\nReceived Activity.Context.SpanId:{ activity.Context.SpanId }");
+            Console.WriteLine($"\nReceived Activity.Context.TraceId:{ activity.Context.TraceId }");
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://new-shalimar.azure-devices.net/twins/new-nabeel-percept-device/modules/edgesolution/methods?api-version=2018-06-30"))
+                {
+                    request.Headers.TryAddWithoutValidation("Authorization", "SharedAccessSignature sr=new-shalimar.azure-devices.net&sig=ubHSFWY8dG9oDD0t%2FUPpQ4b8Rz3wcjrypOWThIeWi2A%3D&se=1637570626&skn=iothubowner");
+
+                    request.Content = new StringContent("{\n    \"methodName\": \"reboot\",\n    \"responseTimeoutInSeconds\": 200,\n    \"payload\": {\n        \"input1\": \"someInput\",\n        \"input2\": \"anotherInput\"\n    }\n}");
+                    request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+                    propagator.Inject(new PropagationContext(activity.Context, Baggage.Current), request, HttpRequestMessageHeaderValueSetter);
+                    var httpResponseMessage = await httpClient.SendAsync(request);
+                    Console.WriteLine($"\nResponse status: {httpResponseMessage.StatusCode}, payload:\n\t{httpResponseMessage.Content}");
+                }
+            }
+
+            
         }
     }
 }
--- EdgeSolution/EdgeSolution/modules/OpenTelemetryExporterModule/main.py
+++ EdgeSolution/EdgeSolution/modules/OpenTelemetryExporterModule/main.py
@@ -4,11 +4,27 @@
 
 import time
 import os
+from azure.core.pipeline import Pipeline
+from azure.core.pipeline.policies import (
+    BearerTokenCredentialPolicy,
+    ContentDecodePolicy,
+    DistributedTracingPolicy,
+    HeadersPolicy,
+    HttpLoggingPolicy,
+    NetworkTraceLoggingPolicy,
+    UserAgentPolicy,
+)
+
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-
+#from opentelemetry.ext import tags
+from opentelemetry.propagate import set_global_textmap
+#from opentelemetry.propagation import Format
 from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
+from opentelemetry.trace.propagation import _SPAN_KEY
+from opentelemetry.context import Context, get_current
+from opentelemetry.propagators.textmap import DefaultGetter
 
 import sys
 import asyncio
@@ -22,32 +38,95 @@ async def main():
     try:
         if not sys.version >= "3.5.3":
             raise Exception( "The sample requires python 3.5.3+. Current version of Python: %s" % sys.version )
-        print ( "Version 1.0.0.11 === IoT Hub Client for Python. M Nabeel Khan." )
+        print ( "Version 1.0.0.48 === IoT Hub Client for Python. M Nabeel Khan." )
         print (os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"])
-        exporter = AzureMonitorTraceExporter.from_connection_string(
-            os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"]
-        )
-
-        print("Created exporter")
-
-        trace.set_tracer_provider(TracerProvider())
-        tracer = trace.get_tracer(__name__)
-        span_processor = BatchSpanProcessor(exporter)
-        trace.get_tracer_provider().add_span_processor(span_processor)
-
-        with tracer.start_as_current_span("hello"):
-            print("Hello, World!")
-
-        print("Done exporter")
-
+        print("printing span")
+        print(_SPAN_KEY)
+        #clientWithHttpPipeline = IoTHubModuleClient(http_pipeline = )
         # The client object is used to interact with your Azure IoT hub.
-        module_client = IoTHubModuleClient.create_from_edge_environment()
+        module_client = IoTHubModuleClient.create_from_edge_environment(websockets = True)
 
         # connect the client.
         await module_client.connect()
 
         # define behavior for receiving an input message on input1
         async def input1_listener(method_request):
+            exporter = AzureMonitorTraceExporter.from_connection_string(
+                os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+            )
+            print("Created exporter within the method")
+            print(trace)            
+
+            trace.set_tracer_provider(TracerProvider())
+            print("__name__")
+            print(__name__)
+            tracer = trace.get_tracer(__name__)
+            print("Getting trace detail")
+            print(tracer)
+            print("Printing what is in tracer")
+            print(tracer.__dict__)
+            print("getting more what is in tracer by going object by object")
+            print("Printing tracer.resource")
+            print(tracer.resource)
+            print("Printing what is in tracer.resource")
+            print(tracer.resource.__dict__)
+            print("Printing tracer.sampler")
+            print(tracer.sampler)
+            print("Printing what is in tracer.sampler")
+            print(tracer.sampler.__dict__)            
+            print("Printing tracer.span_processor")
+            print(tracer.span_processor)
+            print("Printing what is in tracer.span_processor")
+            print(tracer.span_processor.__dict__) 
+            print("Printing tracer.span_processor._span_processors")
+            print(tracer.span_processor._span_processors)
+
+            print("Printing tracer.id_generator")
+            print(tracer.id_generator)
+            print("Printing what is in tracer.id_generator")
+            print(tracer.id_generator.__dict__)   
+
+            print("Printing tracer.instrumentation_info")
+            print(tracer.instrumentation_info)
+
+
+            print("printing noop tracer")
+            #print(tracer._noop_tracer)
+            #print(tracer._noop_tracer.__dict__)
+            # print("Printing what is in trace")
+            # print(trace.__dict__)
+            print(method_request)
+            print("Request object")
+            print(method_request.__dict__)
+            print(type(method_request))
+            print(_SPAN_KEY)
+            #print(method_request.headers)
+            # propagator = tracer.get_propagator()
+            # print("printing propagator")
+            # print(propagator)
+            # span_ctx = tracer.extract(_SPAN_KEY)
+            # print("Printing span context")
+            # print(span_ctx)
+            # print("Printing tags")
+            #print(tags)
+            #span_tags = {tags.SPAN_KIND: tags.SPAN_KIND_RPC_SERVER}
+
+            # span_tags = {}
+            # with tracer.start_span('format', child_of=span_ctx, tags=span_tags):
+            #     hello_to = method_request.args.get('helloTo')
+            #     print(hello_to) #'Hello, %s!' % hello_to
+            
+            span_processor = BatchSpanProcessor(exporter)
+            print("print whats in span_processor")
+            print(span_processor)
+            print(span_processor.__dict__)
+            trace.get_tracer_provider().add_span_processor(span_processor)
+
+            with tracer.start_as_current_span("parent"):
+                print("Hello, World from IoT edge!")
+
+            print("Done exporter within the method")
+
             print(method_request)
             print("the data in the message received on input1 was ")
             print(method_request.name)
